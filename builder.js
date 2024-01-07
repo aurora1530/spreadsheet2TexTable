ac3bbd9ec0db05f8e38ee44e9a26038c1e170836
aurora1530 <k.0523.y.1530@gmail.com>
2024-01-07 16:04:46 +0900

    Refactor file path handling and code replacement in builder.js

--- builder.js
+++ builder.js
@@ -1,42 +1,53 @@
 const fs = require('fs');
 const path = require('path');
 
+const getPathsMatchExt = (dir, ext) => {
+  return fs
+    .readdirSync(dir)
+    .filter((filename) => path.extname(filename) === ext)
+    .map((filename) => path.join(dir, filename));
+};
+
+const readCode = (filePaths) => {
+  return filePaths.map((filePath) => fs.readFileSync(filePath, 'utf8')).join('\n');
+};
+
+const replacePlaceholders = (html, fileType, code) => {
+  const placeHolder = new RegExp(
+    `(<!-- ${fileType}-import-start -->)(.*?)(<!-- ${fileType}-import-end -->)`,
+    's'
+  );
+  const openTag = fileType === 'js' ? '<script>' : '<style>';
+  const closeTag = fileType === 'js' ? '</script>' : '</style>';
+  return html.replace(
+    placeHolder,
+    (_, start, __, end) => `${start}\n${openTag}\n${code}\n${closeTag}\n${end}`
+  );
+};
+
+const createFileListMessage = (filePaths) => {
+  return filePaths.map((filePath) => '└─ ' + path.basename(filePath)).join('\n');
+};
+
 /**
  * Main function that reads HTML, JavaScript, and CSS files, replaces import placeholders with actual code,
  * and writes the modified HTML back to the file.
  */
 function main() {
   const html = fs.readFileSync(path.join(__dirname, './public/index.html'), 'utf-8');
-  const jsPathes = fs
-    .readdirSync(path.join(__dirname, './public/js/'))
-    .filter((filename) => path.extname(filename) === '.js')
-    .map((filename) => path.join(__dirname, './public/js/', filename));
-  const cssPathes = fs
-    .readdirSync(path.join(__dirname, './public/css/'))
-    .filter((filename) => path.extname(filename) === '.css')
-    .map((filename) => path.join(__dirname, './public/css/', filename));
-
-  const jsCode = jsPathes.map((path) => fs.readFileSync(path, 'utf-8')).join('\n');
-  const cssCode = cssPathes.map((path) => fs.readFileSync(path, 'utf-8')).join('\n');
-  const replacedHtml = html
-    .replace(
-      /(<!-- js-import-start -->)(.*?)(<!-- js-import-end -->)/s,
-      (_, start, code, end) => `${start}\n<script>\n${jsCode}\n</script>\n${end}`
-    )
-    .replace(
-      /(<!-- css-import-start -->)(.*?)(<!-- css-import-end -->)/s,
-      (_, start, code, end) => `${start}\n<style>\n${cssCode}\n</style>\n${end}`
-    );
+  const publicPath = path.join(__dirname, './public/');
+  const jsPaths = getPathsMatchExt(path.join(publicPath, 'js/'), '.js');
+  const cssPaths = getPathsMatchExt(path.join(publicPath, 'css/'), '.css');
+  const jsCode = readCode(jsPaths);
+  const cssCode = readCode(cssPaths);
+  let replacedHtml = replacePlaceholders(html, 'js', jsCode);
+  replacedHtml = replacePlaceholders(replacedHtml, 'css', cssCode);
 
   fs.writeFileSync(path.join(__dirname, './public/index.html'), replacedHtml);
   const message =
     `Successfully built HTML file.\n` +
-    `js files:\n${jsPathes
-      .map((filePath) => '└─ ' + path.basename(filePath))
-      .join('\n')}\n` +
-    `css files:\n${cssPathes
-      .map((filePath) => '└─ ' + path.basename(filePath))
-      .join('\n')}`;
+    `js files:\n${createFileListMessage(jsPaths)}\n` +
+    `css files:\n${createFileListMessage(cssPaths)}`;
   console.log(message);
 }
 
